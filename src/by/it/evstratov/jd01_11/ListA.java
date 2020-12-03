28be7e39a65d2a7018c6c60f186c3a3e5c614923
shepard <www.shepard1992@gmail.com>
2020-12-03 11:48:29 +0300

    Homework jd_01_11 started

---
+++ src/by/it/evstratov/jd01_11/ListA.java
@@ -0,0 +1,7 @@
+package by.it.evstratov.jd01_11;
+
+public class ListA<T>{
+
+    
+
+}
---
+++ src/by/it/evstratov/jd01_11/Runner.java
@@ -0,0 +1,14 @@
+package by.it.evstratov.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        ListA<String> list = new ListA<>();
+        
+    }
+
+}
