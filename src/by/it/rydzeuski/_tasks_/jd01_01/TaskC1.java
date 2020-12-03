a96f5e58d6dc70178af4cb3c85a10ea839f92da0
rydzeuski <rydzewski@mail.ru>
2020-12-03 11:45:38 +0300

    homework 01_10

--- src/by/it/rydzeuski/_tasks_/jd01_01/TaskC1.java
+++ src/by/it/rydzeuski/_tasks_/jd01_01/TaskC1.java
@@ -15,9 +15,9 @@ class TaskC1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i=sc.nextInt();
-        int j=sc.nextInt();{
+        int j=sc.nextInt();
 
-            }
+
             System.out.println("Sum" + " " + "=" + " " + (i + j));
         }
 
