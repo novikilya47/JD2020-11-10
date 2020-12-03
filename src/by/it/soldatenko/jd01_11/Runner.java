515da38836c487661b5c1ca29e13ed8bacf98645
ivansoldatenko <ivansoldatenko80@gmail.com>
2020-12-03 12:35:30 +0300

    home work

---
+++ src/by/it/soldatenko/jd01_11/Runner.java
@@ -0,0 +1,9 @@
+package by.it.soldatenko.jd01_11;
+
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> list = new ListA<>();
+    }
+}
