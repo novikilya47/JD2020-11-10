7f5389e6cdc518936a151674576304b0e0bba0a8
Ihar Leshchanka <igor.leshchenko@gmail.com>
2020-12-05 00:22:35 +0300

    jd01_11: TaskB complete

---
+++ src/by/it/leshchenko/jd01_11/ListB.java
@@ -0,0 +1,169 @@
+package by.it.leshchenko.jd01_11;
+
+import java.util.*;
+
+public class ListB<E> implements List<E> {
+
+    @SuppressWarnings("unchecked")
+    private E[] elements = (E[]) new Object[0];
+    private int size = 0;
+
+    @Override
+    public boolean add(E e) {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 3 / 2 + 1);
+        }
+        elements[size++] = e;
+        return true;
+    }
+
+    @Override
+    public E remove(int index) {
+        E element = elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+        elements[size] = null; // for GC
+        return element;
+    }
+
+    @Override
+    public E get(int index) {
+        return elements[index];
+    }
+
+    @Override
+    public E set(int index, E element) {
+        E old = elements[index];
+        elements[index] = element;
+        return old;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 3 / 2 + 1);
+        }
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        for (E e : c) {
+            if (size == elements.length) {
+                elements = Arrays.copyOf(elements, elements.length * 3 / 2 + 1);
+            }
+            elements[size] = e;
+            size++;
+        }
+        return true;
+    }
+
+    // Почему-то через StringJoiner вызывается ошибка java.lang.NullPointerException at by.it.leshchenko.jd01_11.ListB.toString(ListB.java:68)
+//    @Override
+//    public String toString() {
+//        StringJoiner stringJoiner = new StringJoiner(", ", "[", "]");
+//        for (int i = 0; i < size; i++) {
+//            stringJoiner.add(elements[i].toString());
+//        }
+//        return stringJoiner.toString();
+//    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        String delimiter = "";
+        for (int i = 0; i < size; i++) {
+            sb.append(delimiter).append(elements[i]);
+            delimiter = ", ";
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
