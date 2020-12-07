2afa9fa4e0e1a7fadb64142f21d8eca5b7a2aeb9
alexandraskikh <sasha.zavoiskikh@gmail.com>
2020-12-07 21:56:57 +0300

    homework 08 Task A done

--- src/by/it/zavoiskikh/jd01_08/Scalar.java
+++ src/by/it/zavoiskikh/jd01_08/Scalar.java
@@ -17,6 +17,53 @@ class Scalar extends Var {
     }
 
     @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double otherValue = ((Scalar) other).value;
+            double result = this.value + otherValue;
+            return new Scalar(result);
+        } else
+            return other.add(this);
+     }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar) {
+            double otherValue = ((Scalar) other).value;
+            double result = this.value - otherValue;
+            return new Scalar(result);
+        } else
+            return other.sub(this).mul (new Scalar(-1));
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double otherValue = ((Scalar) other).value;
+            double result = this.value * otherValue;
+            return new Scalar(result);
+        } else
+            return other.mul (this);
+    }
+
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar) {
+            double otherValue = ((Scalar) other).value;
+            if (otherValue == 0) {
+System.out.println("Division by zero");
+return null; //stub exception
+            }
+            double result = this.value * otherValue;
+            return new Scalar(result);
+        } else
+            return super.div (other);
+    }
+
+
+
+
+    @Override
     public String toString() {
         return Double.toString(value);
     }
--- src/by/it/zavoiskikh/jd01_08/Var.java
+++ src/by/it/zavoiskikh/jd01_08/Var.java
@@ -1,6 +1,29 @@
 package by.it.zavoiskikh.jd01_08;
 
-abstract class Var {
+abstract class Var implements Operation {
+    @Override
+    public Var add(Var other) {
+        System.out.printf("Operation %s + %s not found\n", this, other);
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.printf("Operation %s - %s not found\n", this, other);
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.printf("Operation %s * %s not found\n", this, other);
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.printf("Operation %s / %s not found\n", this, other);
+        return null;
+    }
 
     @Override
     public String toString() {
