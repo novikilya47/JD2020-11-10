3ad6edc282e65dda24206450aa6b030f1d7d2a24
Ihar Leshchanka <igor.leshchenko@gmail.com>
2020-12-07 20:09:34 +0300

    jd01_12: TaskA complete

---
+++ src/by/it/leshchenko/jd01_12/TaskA1.java
@@ -0,0 +1,33 @@
+package by.it.leshchenko.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class TaskA1 {
+
+    private ArrayList<Integer> grades = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        TaskA1 instance = new TaskA1();
+        for (int i = 0; i < 40; i++) {
+            instance.grades.add(random.nextInt(10) + 1);
+        }
+        System.out.println(instance.grades);
+        instance.clearBad(instance.grades);
+        System.out.println(instance.grades);
+    }
+
+    @SuppressWarnings("Java8CollectionRemoveIf")
+    void clearBad(List<Integer> grades) {
+        Iterator<Integer> iterator = grades.iterator();
+        while (iterator.hasNext()) {
+            Integer v = iterator.next();
+            if (v < 4) {
+                iterator.remove();
+            }
+        }
+    }
+}
---
+++ src/by/it/leshchenko/jd01_12/TaskA2.java
@@ -0,0 +1,43 @@
+package by.it.leshchenko.jd01_12;
+
+import java.util.*;
+
+public class TaskA2 {
+    public static void main(String[] args) {
+
+        Integer[] values1 = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+        Integer[] values2 = {9, 8, 8, 7, 6, 5, 4, 3, 2, 0};
+        List<Integer> integers1 = Arrays.asList(values1);
+        List<Integer> integers2 = Arrays.asList(values2);
+
+        HashSet<Integer> hashSet = new HashSet<>(integers1);
+        TreeSet<Integer> treeSet = new TreeSet<>(integers2);
+
+        System.out.printf("hastSet=%s\n", hashSet);
+        System.out.printf("treeSet=%s\n", treeSet);
+        Set<Integer> union = getUnion(hashSet, treeSet);
+        System.out.printf("union=%s\n", union);
+        Set<Integer> cross = getCross(hashSet, treeSet);
+        System.out.printf("cross=%s\n", treeSet);
+    }
+
+    private static Set<Integer> getCross(Set<Integer> set1, Set<Integer> set2) {
+        HashSet<Integer> result = new HashSet<>(set1);
+        Iterator<Integer> iterator = result.iterator();
+//        while (iterator.hasNext()) {
+//            Integer integer = iterator.next();
+//            if (!set2.contains(integer)) {
+//                iterator.remove();
+//            }
+//        }
+        result.retainAll(set2);
+        return result;
+    }
+
+    private static Set<Integer> getUnion(Set<Integer> set1, Set<Integer> set2) {
+        HashSet<Integer> result = new HashSet<>(set1);
+//        result.addAll(set1);
+        result.addAll(set2);
+        return result;
+    }
+}
