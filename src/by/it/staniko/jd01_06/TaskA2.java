6a6c47d461c1fc265e6ccfcbce417412fb0276a3
Maria <mstanilko.s@mail.ru>
2020-12-08 16:36:25 +0300

    jd01-06 TaskA2 changed

--- src/by/it/staniko/jd01_06/TaskA2.java
+++ src/by/it/staniko/jd01_06/TaskA2.java
@@ -18,7 +18,7 @@ public class TaskA2 {
     }
 
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("[а-яА-яёЁ]+");
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
         Matcher matcher= pattern.matcher(Poem.text);
         while (matcher.find()){
             String word=matcher.group();
