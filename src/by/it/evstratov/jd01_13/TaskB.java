66a5fe2a49b518579c10d333b53de3e8270cd254
shepard <www.shepard1992@gmail.com>
2020-12-08 18:42:07 +0300

    Homework jd_01_13 B completed

--- src/by/it/evstratov/jd01_13/TaskB.java
+++ src/by/it/evstratov/jd01_13/TaskB.java
@@ -13,9 +13,35 @@ public class TaskB {
 
         while (!(str = scanner.nextLine()).equals("END")){
 
-            Double num = Double.parseDouble(str);
-            doubleArrayList.add(num);
-            System.out.println(num);
+            double num = 0d;
+            try{
+                num = Double.parseDouble(str);
+                doubleArrayList.add(num);
+                double sum = 0;
+                for (Double aDouble : doubleArrayList) {
+                    sum+=aDouble;
+                }
+                if(sum <= 0){
+                    throw new ArithmeticException();
+                }
+                double sqrt = Math.sqrt(sum);
+                System.out.println(num+":"+sqrt);
+
+            }catch (NumberFormatException | ArithmeticException e){
+                System.out.println("catch");
+                String name = e.getClass().getName();
+                String nameA = TaskB.class.getName();
+                StackTraceElement[] stackTraceElements = e.getStackTrace();
+                for (StackTraceElement stackTraceElement : stackTraceElements) {
+                    String className = stackTraceElement.getClassName();
+                    if(className.equals(nameA)){
+                        int lineNumber = stackTraceElement.getLineNumber();
+                        System.out.printf(" name: %s\n class: %s\n line: %d\n", name, className, lineNumber);
+                        break;
+                    }
+
+                }
+            }
 
         }
 
