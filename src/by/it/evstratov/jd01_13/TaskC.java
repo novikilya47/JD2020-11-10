dc1aa7bf8c55c88d3ddf9c989fbe35d4bd288315
shepard <www.shepard1992@gmail.com>
2020-12-09 10:06:11 +0300

    Homework jd_01_13 C started

--- src/by/it/evstratov/jd01_13/TaskC.java
+++ src/by/it/evstratov/jd01_13/TaskC.java
@@ -1,25 +1,28 @@
 package by.it.evstratov.jd01_13;
 
-import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TaskC {
 
     static int errors = 0;
-    static Scanner scanner = new Scanner(System.in);
-    static ArrayDeque<Double> arrayDeque = new ArrayDeque<>();
+    static Scanner scanner;
+    static ArrayList<Double> arrayList = new ArrayList<>();
 
-    static void readData() throws InterruptedException{
-        double value = 0;
+    static void readData(){
+        double value;
         try{
             value = Double.parseDouble(scanner.nextLine());
-            arrayDeque.add(value);
+            arrayList.add(value);
         }catch (NumberFormatException e){
             errors++;
-            Thread.sleep(100);
-            Double[] doubles = arrayDeque.toArray(new Double[arrayDeque.size()]);
-            for (int i = doubles.length-1; i >= 0; i--) {
-                System.out.print(doubles[i] + " ");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException interruptedException) {
+                interruptedException.printStackTrace();
+            }
+            for (int i = arrayList.size()-1; i >= 0; i--) {
+                System.out.print(arrayList.get(i) + " ");
             }
             System.out.println();
             if(errors == 5){
@@ -27,14 +30,11 @@ public class TaskC {
             }
         }
     }
+    public static void main(String[] args){
 
-    public static void main(String[] args) throws InterruptedException {
-
-        Scanner scanner = TaskC.scanner;
+        scanner = new Scanner(System.in);
         while (true) {
             readData();
         }
-
     }
-
 }
