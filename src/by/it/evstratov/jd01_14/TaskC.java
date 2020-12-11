ab7ba14d8431f0b4ab86f9820f0a6afbeac6e6f1
shepard <www.shepard1992@gmail.com>
2020-12-11 17:05:48 +0300

    Homework jd_01_14 C is started

--- src/by/it/evstratov/jd01_14/TaskC.java
+++ src/by/it/evstratov/jd01_14/TaskC.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 public class TaskC {
 
+    public static final String FILENAME_TXT_RESULT = "resultTaskC.txt";
+
     private static String getPath(Class<?> clazz) {
         String src = System.getProperty("user.dir") + File.separator + "src" + File.separator;
         String path = clazz.getName()
@@ -14,7 +16,17 @@ public class TaskC {
 
     public static void main(String[] args) {
 
+        String path = getPath(TaskC.class);
+        String secondName = "evstratov";
+        String nameMyPath = path.split(secondName)[0];
+        File myPath = new File(nameMyPath+secondName+File.separator);
+        if(myPath.isDirectory()){
+            File[] files = myPath.listFiles();
+            for (File file : files) {
+                System.out.println(file.getName());
+            }
 
+        }
 
     }
 
