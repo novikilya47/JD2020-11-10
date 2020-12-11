605707321db9381b67b687d2f2790b5e7404ff56
shepard <www.shepard1992@gmail.com>
2020-12-11 15:14:30 +0300

    Homework jd_01_14 B is started

--- src/by/it/evstratov/jd01_14/TaskB.java
+++ src/by/it/evstratov/jd01_14/TaskB.java
@@ -1,10 +1,22 @@
 package by.it.evstratov.jd01_14;
 
+import java.io.File;
+
 public class TaskB {
+
+    private static String getPath(Class<?> clazz) {
+        String src = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String path = clazz.getName()
+                .replace(clazz.getSimpleName(), "")
+                .replace(".", File.separator);
+        return src + path;
+    }
 
     public static void main(String[] args) {
 
+        String fileName = "TaskB.txt";
 
+        System.out.println(getPath(TaskB.class)+fileName);
 
     }
 
