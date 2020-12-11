6b824d3738c913bdf93cafe3672b4809dc93515c
shepard <www.shepard1992@gmail.com>
2020-12-11 14:58:20 +0300

    Homework jd_01_14

--- src/by/it/evstratov/jd01_14/TaskA.java
+++ src/by/it/evstratov/jd01_14/TaskA.java
@@ -1,51 +1,95 @@
 package by.it.evstratov.jd01_14;
 
 import java.io.*;
+import java.util.*;
 
 public class TaskA {
 
-    private static String dir(Class<?> cl){
-        String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
-        String clDir = cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
-        return path+clDir;
-    }
+    public static final String FILENAME_BIN = "dataTaskA.bin";
+    public static final String FILENAME_TXT = "resultTaskA.txt";
+    private static Random random = new Random();
 
     public static void main(String[] args) {
+        String path = getPath(TaskA.class);
+        String filename = path + FILENAME_BIN;
+        writeInt(filename);
+        List<Integer> list = readIntegers(filename);
+        printToConsole(list);
+        printListToFile(path, list);
 
-        DataOutputStream dos = null;
-        try {
-            dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(dir(TaskA.class))));
-            for (int i = 0; i < 20; i++) {
-                dos.writeInt((int)Math.random()*25);
+    }
+
+    private static void printListToFile(String path, List<Integer> list) {
+        String filenameTxt = path + FILENAME_TXT;
+        try (PrintWriter out = new PrintWriter(filenameTxt)) {
+            double sum2 = 0;
+            for (Integer integer : list) {
+                out.print(integer + " ");
+                sum2 += integer;
             }
-        }catch (FileNotFoundException e){
-            e.printStackTrace();
+            double avg2 = sum2 / list.size();
+            out.printf(Locale.ENGLISH, "\navg=%f\n", avg2);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void printToConsole(List<Integer> list) {
+        double sum = 0;
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+            sum += integer;
+        }
+        double avg = sum / list.size();
+        System.out.printf(Locale.ENGLISH,"\navg=%f\n", avg);
+    }
+
+    private static List<Integer> readIntegers(String filename) {
+        List<Integer> list = new ArrayList<>();
+        DataInputStream dataInputStream = null;
+        try {
+            dataInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)));
+            while (dataInputStream.available() > 0) {
+                int value = dataInputStream.readInt();
+                list.add(value);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
-        }finally {
-            if(dos != null){
-                try{
-                    dos.close();
+            throw new RuntimeException(e);
+        } finally {
+            if (Objects.nonNull(dataInputStream)) {
+                try {
+                    dataInputStream.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-        try(DataInputStream inputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(dir(TaskA.class) + "dataTaskA.bin")));
-        PrintWriter out2 = new PrintWriter(new FileWriter(dir(TaskA.class)+"resultTaskA.txt"))){
-            double sum = 0;
-            double count = 0;
-            while (inputStream.available() > 0){
-                int i = inputStream.readInt();
-                out2.print(i + " ");
-                sum += i;
-                count++;
-            }
-            out2.println("\navg="+sum/count);
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        return list;
+    }
 
+    private static void writeInt(String filename) {
+        try (
+                DataOutputStream dataOutputStream = new DataOutputStream(
+                        new BufferedOutputStream(
+                                new FileOutputStream(filename)
+                        )
+                )
+        ) {
+            for (int i = 0; i < 20; i++) {
+                int value = 123 + random.nextInt(1000);
+                dataOutputStream.writeInt(value);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getPath(Class<?> clazz) {
+        String src = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String path = clazz.getName()
+                .replace(clazz.getSimpleName(), "")
+                .replace(".", File.separator);
+        return src + path;
     }
 
 }
