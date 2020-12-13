93636e1601c04cec0194fbba7124e3bfd90af442
shepard <www.shepard1992@gmail.com>
2020-12-13 12:42:55 +0300

    Homework jd_01_15 B is started

--- src/by/it/evstratov/jd01_15/TaskB.java
+++ src/by/it/evstratov/jd01_15/TaskB.java
@@ -1,12 +1,13 @@
 package by.it.evstratov.jd01_15;
-
+//public class TaskB
 /**
- * This is a JavaDoc class comment
+ * This is a JavaDoc comment
  */
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 //public class TaskB
 
@@ -14,11 +15,20 @@ public class TaskB {
 
     //static method getPath
 
-    private static String getPath(Class<?> clazz) {
+    private static final String TASK_B = "TaskB.txt";
+
+    private static String getPathJava(Class<?> clazz) {
         String src = System.getProperty("user.dir") + File.separator + "src" + File.separator;
         String path = clazz.getName()
                 .replace(".", File.separator);
         return src + path + ".java";
+    }
+    private static String getPath(Class<?> clazz) {
+        String src = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String path = clazz.getName()
+                .replace(clazz.getSimpleName(), "")
+                .replace(".", File.separator);
+        return src + path;
     }
 
 
@@ -28,18 +38,57 @@ public class TaskB {
 
     public static void main(String[] args) {
 
-        try(FileReader fr = new FileReader(getPath(TaskB.class))){
+        try(FileReader fr = new FileReader(getPathJava(TaskB.class))){
 
             /*
                Установка флагов для отлавливания комментов
             */
 
-            boolean isCommentOpen;
+            boolean isCommentOpen = false;
+            boolean isLineComment = false;
+            boolean isDocComment = false;
+            StringBuilder fileToStr = new StringBuilder();
+
             while (fr.ready()){
                 char character = (char) fr.read();
                 String charToString = character+"";
-                System.out.println(charToString);
+
+                if(!isCommentOpen){
+                    if(charToString.equals("/")){
+                        isCommentOpen = true;
+                    }else{
+                        fileToStr.append(charToString);
+                    }
+                }else{
+                    if(isLineComment){
+                        if(charToString.equals("\n")) {
+                            isLineComment = false;
+                            isCommentOpen = false;
+                            fileToStr.delete(fileToStr.length()-1, fileToStr.length());
+                        }
+                    }else if(isDocComment){
+                        if(charToString.equals("/")) {
+                            isDocComment = false;
+                            isCommentOpen = false;
+                            fileToStr.delete(fileToStr.length()-1, fileToStr.length());
+                        }
+                    }else{
+                        if(charToString.equals("/")){
+                            isLineComment = true;
+                        }else if(charToString.equals("*")){
+                            isDocComment = true;
+                        }else{
+                            isCommentOpen = false;
+                            fileToStr.append("/").append(charToString);
+                        }
+                    }
+                }
+
             }
+            System.out.println(fileToStr);
+            PrintWriter pr = new PrintWriter(getPath(TaskB.class)+TASK_B);
+            pr.write(String.valueOf(fileToStr));
+            pr.close();
         }catch (IOException e) {
             e.printStackTrace();
         }
