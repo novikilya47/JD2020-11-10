cd860b5203ea05cbcb24cf20fdcbc506f3b0b079
alexandraskikh <sasha.zavoiskikh@gmail.com>
2020-12-13 17:04:12 +0300

    homework 09 Task A done

---
+++ src/by/it/zavoiskikh/jd01_09/Patterns.java
@@ -0,0 +1,11 @@
+package by.it.zavoiskikh.jd01_09;
+
+interface Patterns {
+
+    String OPERATION = "[-+*/]";
+    String SCALAR="-?[0-9]+(\\.[0-9]+)?";
+    String VECTOR="\\{"+SCALAR+"(,"+SCALAR + ")*}";
+    String MATRIX="\\{"+VECTOR+"(,"+VECTOR + ")*}";
+    //1 2 3.5 4.775 -8 -99
+
+}
