11b8f61f07175d9ac9d9630c841acc4c135bece0
Littledrop777 <Littledrop777@gmail.com>
2020-12-14 13:52:39 +0300

    Homework jd01_15 TaskB edit

--- src/by/it/plehanova/jd01_15/TaskB.java
+++ src/by/it/plehanova/jd01_15/TaskB.java
@@ -1,3 +1,4 @@
+
 package by.it.plehanova.jd01_15;
 
 /*Класс TaskB
@@ -5,12 +6,76 @@ package by.it.plehanova.jd01_15;
  Программа должна прочитать свой собственный текст в переменную типа StringBuilder.
  Читая программу необходимо удалить все возможные виды комментариев, не трогая остальное.
  Результат вывести на консоль и в файл с тем же местоположением и именем, но с расширением txt, а не java.
- Считайте, что комментарии внутри строки невозможны (s=“//так не бывает”) и внутри комментария тоже
-/** bla... /*так не бывает*/  //и так тоже bla... */ . Регулярные выражения использовать нельзя */
+ Считайте, что комментарии внутри строки невозможны (s=“//так не бывает”) и внутри комментария тоже.
+Регулярные выражения использовать нельзя */
 
+import java.io.*;
 
 public class TaskB {
-    public static void main(String[] args) {
+    public static final String TaskB_JAVA = "TaskB.java";
+    public static final String TaskB_TXT = "TaskB.txt";
 
+    public static void main(String[] args) {
+        /*
+         * тестовый многострочный комментарий
+         */
+        String javaPath = getPath(TaskB.class) + TaskB_JAVA;
+        String txtPath = getPath(TaskB.class) + TaskB_TXT;
+        StringBuilder str = deleteComments(javaPath);
+        try (PrintWriter printWriter = new PrintWriter(txtPath)) {
+            printWriter.print(str);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    //читаем в переменную и удаляем комментарии
+    private static StringBuilder deleteComments(String javaPath) {
+        try (FileReader fileReader = new FileReader(javaPath)) {
+            StringBuilder text = new StringBuilder();
+            int indexStart = 0;
+            int indexEnd = 0;
+
+            while (fileReader.ready()) {
+                char currentChar = (char) fileReader.read();
+                text.append(currentChar);
+                if (currentChar == '/') {
+                    char nextChar = (char) fileReader.read();
+                    text.append(nextChar);
+                    if (nextChar == '*') {
+                        indexStart = text.length() - 2;
+                    } else if (nextChar == '/') {
+                        text.delete(text.length() - 2, text.length());
+                        while (fileReader.read() != '\n') {
+
+                        }
+                    }
+                }
+                if (currentChar == '*') {
+                    char nextChar = (char) fileReader.read();
+                    text.append(nextChar);
+                    if (nextChar == '/') {
+                        indexEnd = text.length();
+                    }
+                }
+                if (indexStart > 0 && indexEnd > 0 && indexStart < indexEnd) {
+                    text.delete(indexStart, indexEnd);
+                    indexStart = 0;
+                    indexEnd = 0;
+                }
+            }
+            return text;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //получаем путь к файлу
+    private static String getPath(Class<?> clazz) {
+        String src = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String path = clazz.getPackageName().replace(".", File.separator).concat(File.separator);
+        return src + path;
     }
 }
