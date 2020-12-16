adccb035bba2b35a1fd2cf02ca14f94d10433078
shepard <www.shepard1992@gmail.com>
2020-12-16 15:19:39 +0300

    Homework jd_02_01 calc is started

---
+++ src/by/it/evstratov/calc/RepoVar.java
@@ -0,0 +1,52 @@
+package by.it.evstratov.calc;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class RepoVar {
+
+    private RepoVar() {
+    }
+
+    private static String getFilename() {
+        String src = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String path = RepoVar.class.getName()
+                .replace(RepoVar.class.getSimpleName(), "")
+                .replace(".", File.separator);
+        return src + path;
+    }
+
+    static void loadVariables() {
+        Parser parser = new Parser();
+        try {
+            Files.lines(Paths.get(getFilename()+"vars.txt")).forEach(expression -> {
+                        try {
+                            parser.calc(expression);
+                        } catch (CalcException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+            );
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+    }
+
+
+    @SuppressWarnings("SameParameterValue")
+    static void saveVariables(Map<String, Var> vars) {
+        try (
+                PrintWriter printWriter = new PrintWriter(getFilename()+"vars.txt")) {
+            for (Map.Entry<String, Var> entry : vars.entrySet()) {
+                printWriter.println(entry.getKey() + "=" + entry.getValue());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
