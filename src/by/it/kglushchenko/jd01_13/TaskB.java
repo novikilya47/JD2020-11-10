d4892aa09cf61b8cc30f93e6636608cde7bf22a9
Konstantinchik <kgluschenko83@gmail.com>
2020-12-16 23:02:42 +0300

    calc + jd02_01 A +  13 AB

--- src/by/it/kglushchenko/jd01_13/TaskB.java
+++ src/by/it/kglushchenko/jd01_13/TaskB.java
@@ -3,50 +3,44 @@ package by.it.kglushchenko.jd01_13;
 import java.util.Scanner;
 
 public class TaskB {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Double value=0.0, sum = 0.0, Sq = 0.0;
+        Double value = 0.0, sum = 0.0, Sq = 0.0;
 
         String inputString;
-        while (!((inputString = scanner.nextLine().trim()).equals("end"))) {
+        //while (!((inputString = scanner.nextLine().trim()).equals("END"))) {
+        while (!(inputString = scanner.nextLine()).equals("END")) {
             try {
                 value = Double.valueOf(inputString);
-            } catch (NumberFormatException e) {
-                String name = e.getClass().getName();
-                String nameA = TaskB.class.getName();
-                StackTraceElement[] stackTrace = e.getStackTrace();
-                for (StackTraceElement stackTraceElement : stackTrace) {
-                    String className = stackTraceElement.getClassName();
-                    int lineNumber = stackTraceElement.getLineNumber();
-                    if (className.equals(nameA)) {
-                        System.out.printf(" name: %s\n" +
-                                "class: %s\n" +
-                                " line: %d\n ", name, className, lineNumber);
-                        break;
-                    }
-                }
-            }// catch - NumberFormatException
-            sum += value;
+                System.out.println(value);  // test
 
-            try {
+                sum += value;
+
+                if (sum <= 0) {
+                    throw new ArithmeticException();
+                }
+
                 Sq = Math.sqrt(sum);
-            } catch (ArithmeticException e) {
+                System.out.println(value + " Sqrt sum" + Sq);
+
+            } catch (NumberFormatException | ArithmeticException e) {
                 String name = e.getClass().getName();
-                String nameA = TaskB.class.getName();
-                StackTraceElement[] stackTrace = e.getStackTrace();
-                for (StackTraceElement stackTraceElement : stackTrace) {
+                String nameA = by.it.kglushchenko.jd01_13.TaskB.class.getName();
+                StackTraceElement[] stackTraceElements = e.getStackTrace();
+                for (StackTraceElement stackTraceElement : stackTraceElements) {
                     String className = stackTraceElement.getClassName();
-                    int lineNumber = stackTraceElement.getLineNumber();
                     if (className.equals(nameA)) {
-                        System.out.printf(" name: %s\n" +
-                                "class: %s\n" +
-                                " line: %d\n ", name, className, lineNumber);
+                        int lineNumber = stackTraceElement.getLineNumber();
+                        System.out.printf(" name: %s\n " +
+                                "class: %s\n " +
+                                " line: %d\n", name, className, lineNumber);
                         break;
                     }
+
                 }
-            }// catch - ArithmeticException
-            System.out.printf("%f %f \n", value, Sq);
+            }
         }
     }
 }
--- src/by/it/kglushchenko/jd01_14/TaskB.java
+++ src/by/it/kglushchenko/jd01_14/TaskB.java
@@ -8,7 +8,7 @@ public class TaskB {
 
     public static final String FILENAME_SRC_TXT = "TaskB.txt";
     public static final String FILENAME_DST_TXT = "resultTaskB.txt";
-
+/*
     public static void main(String[] args) {
         int words = 0, punctmarks = 0;
 //        Map<Integer,String>  hm=new Hashtable<>();
@@ -19,7 +19,7 @@ public class TaskB {
 //        }
 //hm.Entry
 
-
+/*
         String path = getPath(TaskB.class);
         try (BufferedReader fileRead = new BufferedReader(new FileReader(path + FILENAME_SRC_TXT))) {
             Pattern pattern = Pattern.compile("([а-яА-яёЁ]+)|(-,.:\\s]+)"); // "([а-яА-яёЁ]+)|([^а-яА-яёЁ\\s]+)"
@@ -57,6 +57,98 @@ public class TaskB {
                 .replace(".", File.separator);
         return src + path;
     }
+*/
+///////////////////////////////////////////////////////////////////////////////////////
+/*
+    public static final String FILENAME_TEXT = "TaskB.txt";
+    public static final String FILENAME_TXT_RESULT = "resultTaskB.txt";
 
+    private static String getPath(Class<?> clazz) {
+        String src = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String path = clazz.getName()
+                .replace(clazz.getSimpleName(), "")
+                .replace(".", File.separator);
+        return src + path;
+    }
+
+    public static void main(String[] args) {
+
+        int countWords = 0;
+        int countMarks = 0;
+        int lengthWord = 0;
+
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(getPath(by.it.evstratov.jd01_14.TaskB.class)+FILENAME_TEXT));
+            while (inputStreamReader.ready()){
+                char character = Character.valueOf((char)inputStreamReader.read());
+                String charToString = character+"";
+
+                if((charToString).matches("[!|?|,|.|;|:]|(\\s*(?<!\\.)\\.(?!\\.)\\s*)")){
+                    countMarks++;
+                }
+                if(charToString.matches("[а-яА-ЯёЁ]")){
+                    lengthWord++;
+                }
+                if(charToString.equals(" ") || charToString.matches("[!|?|,|.|;|:]|(\\s*(?<!\\.)\\.(?!\\.)\\s*)") || charToString.equals("\n")){
+                    if(lengthWord != 0){
+                        lengthWord = 0;
+                        countWords++;
+                    }
+                }
+            }
+            String result = "words="+ countWords+", punctuation marks="+countMarks;
+            System.out.println(result);
+            inputStreamReader.close();
+
+            OutputStreamWriter outputStream = new OutputStreamWriter(new FileOutputStream(getPath(by.it.evstratov.jd01_14.TaskB.class)+FILENAME_TXT_RESULT));
+            outputStream.append(result);
+            outputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+ */
+////////////////////////////////////////////////////////////////////////////////////
+public static void main(String[] args) {
+    String path = getPath(TaskB.class);
+    int words = 0;
+    int marks = 0;
+    try (BufferedReader fileRead = new BufferedReader(new FileReader(path + "TaskB.txt"))) {
+        Pattern pattern = Pattern.compile("([а-яА-яёЁ]+)|([^а-яА-яёЁ\\s]+)");
+        while (fileRead.ready()) {
+            String line = fileRead.readLine();
+            Matcher matcher = pattern.matcher(line);
+            while (matcher.find()) {
+                if (matcher.group(1) != null) {
+                    words++;
+                }
+                if (matcher.group(2) != null) {
+                    marks++;
+                }
+            }
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    System.out.println("words=" + words + " punctuation marks=" + marks);
+
+    try (BufferedWriter fileWrite = new BufferedWriter(new FileWriter(path + "resultTaskB.txt"))) {
+        fileWrite.write("words=" + words + " punctuation marks=" + marks);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+    private static String getPath(Class<?> clazz) {
+        String src = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String path = clazz.getName()
+                .replace(clazz.getSimpleName(), "")
+                .replace(".", File.separator);
+        return src + path;
+    }
 
 }
