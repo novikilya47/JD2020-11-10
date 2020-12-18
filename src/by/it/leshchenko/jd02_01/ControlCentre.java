6ee1f720ea600ae02bf683c4887ea4cc9ca8300d
Ihar Leshchanka <igor.leshchenko@gmail.com>
2020-12-18 12:41:20 +0300

    jd02_01: TaskB complete

--- src/by/it/leshchenko/jd02_01/ControlCentre.java
+++ src/by/it/leshchenko/jd02_01/ControlCentre.java
@@ -4,7 +4,7 @@ public class ControlCentre {
     public static final int MARKET_OPENTIME = 120; // seconds
     public static final int MARKET_SPEED = 100; // (default - 1)
 
-    public static final int MARKET_ENTRY_TIMEOUT = 1000; // milisecond (default - 1000)
-    public static final int BUYER_CHOOSE_TIME_MIN = 500; // milisecond (default - 500)
-    public static final int BUYER_CHOOSE_TIME_MAX = 2000; // milisecond (default - 500)
+    public static final int MARKET_ENTRY_TIMEOUT = 1000; // millisecond (default - 1000)
+    public static final int BUYER_CHOOSE_TIME_MIN = 500; // millisecond (default - 500)
+    public static final int BUYER_CHOOSE_TIME_MAX = 2000; // millisecond (default - 500)
 }
