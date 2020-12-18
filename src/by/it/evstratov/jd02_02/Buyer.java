753332a68f521e9000c507eb8ad3072de08effef
shepard <www.shepard1992@gmail.com>
2020-12-18 10:24:44 +0300

    Homework jd_02_02 B is started

--- src/by/it/evstratov/jd02_02/Buyer.java
+++ src/by/it/evstratov/jd02_02/Buyer.java
@@ -73,17 +73,7 @@ class Buyer extends Thread implements IBuyer, IUseBasket {
         this.setRunnable(false);
         synchronized (this){
             QueueBuyers.add(this);
-            int buyersInQueue = QueueBuyers.getSize();
-            int openCashiers = Cashier.getOpenCashiers();
-            int needToOpenCashiers =(int) Math.ceil((double) buyersInQueue / 5.0);
-            if(openCashiers < needToOpenCashiers){
-                for (int i = 0; i < needToOpenCashiers - openCashiers; i++) {
-                    Cashier cashier = new Cashier(QueueCashiers.getSize());
-                    Thread thread = new Thread(cashier);
-                    QueueCashiers.add(thread);
-                    thread.start();
-                }
-            }
+            Dispatcher.openNeedCashiers();
             while (!this.isRunnable) {
                 try {
                     this.wait();
--- src/by/it/evstratov/jd02_02/Dispatcher.java
+++ src/by/it/evstratov/jd02_02/Dispatcher.java
@@ -23,4 +23,18 @@ public class Dispatcher {
     static boolean marketIsClosed(){
         return buyersCompleted == PLAN;
     }
+
+    static synchronized void openNeedCashiers(){
+        int buyersInQueue = QueueBuyers.getSize();
+        int openCashiers = Cashier.getOpenCashiers();
+        int needToOpenCashiers =(int) Math.ceil((double) buyersInQueue / 5.0);
+        if(openCashiers < needToOpenCashiers){
+            for (int i = 0; i < needToOpenCashiers - openCashiers; i++) {
+                Cashier cashier = new Cashier(QueueCashiers.getSize());
+                Thread thread = new Thread(cashier);
+                QueueCashiers.add(thread);
+                thread.start();
+            }
+        }
+    }
 }
