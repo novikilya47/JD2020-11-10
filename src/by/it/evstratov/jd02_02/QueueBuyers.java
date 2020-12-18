19c63beb4246e14469a596315d04cbf251690df3
shepard <www.shepard1992@gmail.com>
2020-12-18 15:18:34 +0300

    Homework jd_02_02 B and C is started

--- src/by/it/evstratov/jd02_02/QueueBuyers.java
+++ src/by/it/evstratov/jd02_02/QueueBuyers.java
@@ -6,17 +6,26 @@ import java.util.Deque;
 public class QueueBuyers {
 
    private static final Deque<Buyer> deque = new ArrayDeque<>();
+    private static final Deque<Buyer> dequePensioners = new ArrayDeque<>();
 
    static synchronized void add(Buyer buyer){
-       deque.addLast(buyer);
+       if(buyer.isPensioneer()){
+           dequePensioners.addLast(buyer);
+       }else{
+           deque.addLast(buyer);
+       }
    }
 
     static synchronized Buyer extract(){
-        return deque.pollFirst();
+        if(dequePensioners.size() > 0){
+            return dequePensioners.pollFirst();
+        }else{
+            return deque.pollFirst();
+        }
     }
 
     static synchronized int getSize(){
-       return deque.size();
+       return deque.size() + dequePensioners.size();
     }
 
 }
