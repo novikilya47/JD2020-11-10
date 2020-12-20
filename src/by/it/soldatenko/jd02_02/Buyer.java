b1fa410673fe54317c938ff02cd6f2df64f5688c
ivansoldatenko <ivansoldatenko80@gmail.com>
2020-12-20 23:39:22 +0300

    hw 0202

--- src/by/it/soldatenko/jd02_02/Buyer.java
+++ src/by/it/soldatenko/jd02_02/Buyer.java
@@ -1,8 +1,12 @@
 package by.it.soldatenko.jd02_02;
 
 
+import java.util.HashMap;
+
 class Buyer extends Thread implements IBuyer, IUseBasket {
+
     private boolean isRunnable;
+    private static HashMap<String, Integer> set = new HashMap<>();
 
     public void setRunnable(boolean runnable) {
         this.isRunnable = runnable;
@@ -52,12 +56,13 @@ class Buyer extends Thread implements IBuyer, IUseBasket {
             speed = 1.5;
         }
         System.out.println(this + " started to choose goods");
-        int goodsAmount = Helper.getRandom(1, 4);
-        for (int i = 1; i <= goodsAmount; i++) {
-            Helper.sleep((int) speed * Helper.getRandom(500, 2000));
-
-            putGoodsToBasket();
-        }
+//        int goodsAmount = Helper.getRandom(1, 4);
+//        for (int i = 1; i <= goodsAmount; i++) {
+//            Helper.sleep((int) speed * Helper.getRandom(500, 2000));
+//
+//            putGoodsToBasket();
+//
+//        }
         System.out.println(this + " finishded to choose goods");
     }
 
@@ -85,7 +90,7 @@ class Buyer extends Thread implements IBuyer, IUseBasket {
 
     @Override
     public void putGoodsToBasket() {
-        System.out.println(this + " puted " + Good.getGood() + " to the Basket");
+//        System.out.println(this + " puted " + Good.getGood()[0] + " to the Basket");
 
     }
 
--- src/by/it/soldatenko/jd02_02/Cashier.java
+++ src/by/it/soldatenko/jd02_02/Cashier.java
@@ -1,12 +1,18 @@
 package by.it.soldatenko.jd02_02;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cashier implements Runnable {
     private final int number;
+
 
     public Cashier(int number) {
         this.number = number;
     }
 
+
+    public static String[] setS = new String[2];
 
     @Override
     public void run() {
@@ -15,10 +21,18 @@ public class Cashier implements Runnable {
         while (!Dispetcher.marketIsClosed()) {
             Buyer buyer = QueueBuyers.extract();
             if (buyer != null) {
-                System.out.println(this + "started service for" + buyer);
+                System.out.println(this + "started service for " + buyer);
                 int t = Helper.getRandom(2000, 5000);
                 Helper.sleep(t);
-                System.out.println(this + "finished service for" + buyer);
+                HashMap<String, Integer> set = new HashMap<>();
+                set = Basket.getBasket();
+                int sum = 0;
+                for (Map.Entry entry : set.entrySet()) {
+                    System.out.println(this + " " + entry.getKey() + " by price " + entry.getValue() + " " + buyer);
+                    sum = sum + (int) entry.getValue();
+                }
+                System.out.println(this + " recipe " + sum + " " + buyer);
+                System.out.println(this + "finished service for " + buyer);
                 synchronized (buyer) {
                     buyer.setRunnable(true);
                     buyer.notify();
