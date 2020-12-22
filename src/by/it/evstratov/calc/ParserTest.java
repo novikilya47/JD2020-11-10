c816f269d091beb584142058cef3f66480ea883e
shepard <www.shepard1992@gmail.com>
2020-12-22 15:17:28 +0300

    Homework jd_02_04 A is completed

--- src/by/it/evstratov/calc/ParserTest.java
+++ src/by/it/evstratov/calc/ParserTest.java
@@ -30,6 +30,10 @@ public class ParserTest {
         assertEquals(7.3, actual, 1e-5);
         actual = Double.parseDouble(parser.calc("B=A*3.5").toString());
         assertEquals(25.55, actual, 1e-5);
+        actual = Double.parseDouble(parser.calc("B1=B+0.11*-5").toString());
+        assertEquals(25, actual, 1e-5);
+        actual = Double.parseDouble(parser.calc("B2=A/2-1").toString());
+        assertEquals(2.65, actual, 1e-5);
     }
 
     @Test
