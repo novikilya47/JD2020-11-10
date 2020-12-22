be0d11ff127497872bd990c874a89bffb3790b6f
Konstantinchik <kgluschenko83@gmail.com>
2020-12-22 12:55:59 +0300

    jd02_04 start

---
+++ src/by/it/kglushchenko/jd02_04/Parser.java
@@ -0,0 +1,45 @@
+package by.it.kglushchenko.jd02_04;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+    public Var calc(String expression) throws CalcException {
+        // 3 + 4
+        // 7
+        expression = expression.replaceAll("\\s+", ""); // заменяем группу пробелов одним
+        String[] part = expression.split(Patterns.OPERATION, 2); //
+
+
+        if(part.length < 2){
+            return Var.createVar(expression);
+        }
+
+        // A=9
+        Var right = Var.createVar(part[1]);
+        if(expression.contains("=")){
+            // в операнде 0 содержится имя переменной
+            return Var.saveVar(part[0],right); // ключ - операнд 0, значение - значение правой части
+        }
+        Var left = Var.createVar(part[0]);
+        if (Objects.nonNull(left) && Objects.nonNull(right)) {
+            Pattern compile = Pattern.compile(Patterns.OPERATION);
+            Matcher matcher = compile.matcher(expression);
+            if (matcher.find()) {
+                String operation = matcher.group();
+                switch (operation) {
+                    case "+":
+                        return left.add(right);
+                    case "-":
+                        return left.sub(right);
+                    case "*":
+                        return left.mul(right);
+                    case "/":
+                        return left.div(right);
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ src/by/it/kglushchenko/jd02_04/Printer.java
@@ -0,0 +1,12 @@
+package by.it.kglushchenko.jd02_04;
+
+import java.util.Objects;
+
+public class Printer {
+    public void print(Var result){
+        if(Objects.nonNull(result)){
+            System.out.println(result);
+        }
+    }
+
+}
