5db5a310e52ebdfdde647ea97bad75c51be6a2e6
shepard <www.shepard1992@gmail.com>
2020-12-23 07:46:07 +0300

    Homework jd_02_04 all is completed

--- src/by/it/evstratov/calc/ParserTestMatrix.java
+++ src/by/it/evstratov/calc/ParserTestMatrix.java
@@ -49,24 +49,33 @@ public class ParserTestMatrix {
 
     @Test
     public void mulTest() throws CalcException {
-        Scalar scalar = (Scalar)parser.calc("{2,6}*{0,2}");
-        double actual = scalar.getValue();
-        assertEquals (12, actual, 1e-5);
-        Vector vector = (Vector)parser.calc("{2,6}*5");
-        double[] expected = {10,30};
-        double[] actual2 = vector.getValue();
-        assertArrayEquals (expected, actual2, 1e-5);
+        Matrix matrix = (Matrix)parser.calc("{{1,2,3},{4,5,6}}*3");
+        double[][] actual = matrix.getValue();
+        double[][] expected = {{3,6,9},{12,15,18}};
+        for (int i = 0; i < expected.length; i++) {
+            assertArrayEquals(expected[i], actual[i], 1e-5);
+        }
+        matrix = (Matrix)parser.calc("{{1,2,3},{4,5,6}}*{{1,2,3},{4,5,6},{4,5,6}}");
+        actual = matrix.getValue();
+        expected = new double[][]{{21.0, 27.0, 33.0}, {48.0, 63.0, 78.0}};
+        for (int i = 0; i < expected.length; i++) {
+            assertArrayEquals(expected[i], actual[i], 1e-5);
+        }
+        Vector vector = (Vector)parser.calc("{{1,2,3},{4,5,6}}*{1,2,3}");
+        double[] actual1 = vector.getValue();
+        double[] expected1 = new double[]{14.0, 32.0};
+        assertArrayEquals(expected1, actual1, 1e-5);
     }
 
     @Test
     public void expression() throws CalcException {
-        Matrix matrix = (Matrix)parser.calc("M={{1,2,3},{4,5,6}}*2");
+        Matrix matrix = (Matrix)parser.calc("{{1,2,3},{4,5,6}}*2");
         double[][] actual = matrix.getValue();
         double[][] expected = {{2,4,6},{8,10,12}};
         for (int i = 0; i < expected.length; i++) {
             assertArrayEquals(expected[i], actual[i], 1e-5);
         }
-        matrix = (Matrix)parser.calc("M={{1,2,3},{4,5,6}}*2+4");
+        matrix = (Matrix)parser.calc("{{1,2,3},{4,5,6}}*2+4");
         actual = matrix.getValue();
         expected = new double[][]{{6, 8, 10}, {12, 14, 16}};
         for (int i = 0; i < expected.length; i++) {
