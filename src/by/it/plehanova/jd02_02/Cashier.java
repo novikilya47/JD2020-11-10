cfd54641186cb4ee49f41d8ba6f410959e623db7
Littledrop777 <Littledrop777@gmail.com>
2020-12-23 22:41:59 +0300

    jd02_02

--- src/by/it/plehanova/jd02_02/Cashier.java
+++ src/by/it/plehanova/jd02_02/Cashier.java
@@ -21,12 +21,13 @@ public class Cashier implements Runnable {
         while (!Dispatcher.markedIsClosed()) {
             Buyer buyer = QueueBuyers.extract();
             if (buyer != null) {
-
-                System.out.println(this + "Started service for" + buyer);
-                int t = Helper.getRandom(2000, 5000);
-                Helper.sleep(t);
-                printReceipt(buyer);
-                System.out.println(this + "finished service for" + buyer);
+                synchronized (System.in) {
+                    System.out.println(this + "Started service for" + buyer);
+                    int t = Helper.getRandom(2000, 5000);
+                    Helper.sleep(t);
+                    printReceipt(buyer);
+                    System.out.println(this + "finished service for" + buyer);
+                }
                 //noinspection SynchronizationOnLocalVariableOrMethodParameter
                 synchronized (buyer) {
                     buyer.setRunnable(true);
@@ -49,23 +50,25 @@ public class Cashier implements Runnable {
     }
 
     public synchronized void printReceipt(Buyer buyer) {
-
-        ArrayList<Good> goods = buyer.getBasket().getGoodsInBasket();
-        String nameOfGood;
-        double priceOfGood;
-        double totalSum = 0;
-        System.out.printf("%25s\n", "---------------------");
-        System.out.printf("%25s\n", "Receipt of " + buyer);
-        System.out.printf("%25s\n", "---------------------");
-        for (Good good : goods) {
-            nameOfGood = good.getName();
-            priceOfGood = good.getPrice();
-            totalSum += priceOfGood;
-            System.out.printf("%15s | %-3.2f\n", nameOfGood, priceOfGood);
+        synchronized (System.in) {
+            ArrayList<Good> goods = buyer.getBasket().getGoodsInBasket();
+            String nameOfGood;
+            double priceOfGood;
+            double totalSum = 0;
+            int x = 25;
+            System.out.printf("%25s\n", "---------------------");
+            System.out.printf("%25s\n", "Receipt of " + buyer);
+            System.out.printf("%25s\n", "---------------------");
+            for (Good good : goods) {
+                nameOfGood = good.getName();
+                priceOfGood = good.getPrice();
+                totalSum += priceOfGood;
+                System.out.printf("%15s | %-3.2f\n", nameOfGood, priceOfGood);
+            }
+            System.out.printf("%25s\n", "---------------------");
+            System.out.printf("%15s | %-3.2f\n", "total sum", totalSum);
+            System.out.printf("%25s\n", "---------------------");
         }
-        System.out.printf("%25s\n", "---------------------");
-        System.out.printf("%15s | %-3.2f\n", "total sum", totalSum);
-        System.out.printf("%25s\n", "---------------------");
     }
 
     @Override
