150cf480c5eab305690b528ab2edc6cbff905440
Ihar Leshchanka <igor.leshchenko@gmail.com>
2020-12-24 00:25:04 +0300

    jd02_04: TaskA

--- src/by/it/leshchenko/calc/Parser.java
+++ src/by/it/leshchenko/calc/Parser.java
@@ -1,38 +1,76 @@
 package by.it.leshchenko.calc;
 
-import java.util.Objects;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
+    private static final Map<String, Integer> map = new HashMap<>() {
+        private static final long serialVersionUID = -1589178250133784261L;
+        {
+            this.put("=", 0);
+            this.put("-", 1);
+            this.put("+", 1);
+            this.put("*", 2);
+            this.put("/", 2);
+        }
+    };
+
     public Var calc(String expression) throws CalcException {
         expression = expression.trim().replaceAll("\\s+", "");
-        String[] part = expression.split(Patterns.OPERATION, 2);
-        if (part.length < 2) {
-            return Var.createVar(expression);
+        final Matcher expressionInBrackets = Pattern.compile(Patterns.BRACKETS).matcher(expression);
+
+        final List<String> operands = new ArrayList<>(Arrays.asList(expression.split(Patterns.OPERATION)));
+        final Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(expression);
+        List<String> operations = new ArrayList<>();
+        while (matcher.find()) {
+            operations.add(matcher.group());
         }
-        Var right = Var.createVar(part[1]);
-        if (expression.contains("=")) {
-            return Var.save(part[0], right);
+        while (operations.size() > 0) {
+            int index = getIndex(operations);
+            String left = operands.remove(index);
+            String right = operands.remove(index);
+            String operation = operations.remove(index);
+            final Var result = calcOneOperation(left, operation, right);
+            operands.add(index, result.toString());
         }
-        Var left = Var.createVar(part[0]);
-        if (Objects.nonNull(left) && Objects.nonNull(right)) {
-            Pattern compile = Pattern.compile(Patterns.OPERATION);
-            Matcher matcher = compile.matcher(expression);
-            if (matcher.find()) {
-                String operation = matcher.group();
-                switch (operation) {
-                    case "+":
-                        return left.add(right);
-                    case "-":
-                        return left.sub(right);
-                    case "*":
-                        return left.mul(right);
-                    case "/":
-                        return left.div(right);
-                }
+        return Var.createVar(operands.get(0));
+    }
+
+//    public Var calcBrackets(String expression) throws CalcException {
+//        expression = expression.trim().replaceAll("\\(", "").replaceAll("\\)", "");
+//
+//    }
+
+        private int getIndex(List<String> operations) {
+        int index = -1;
+        int priority = -1;
+        for (int i = 0, operationsSize = operations.size(); i < operationsSize; i++) {
+            String operation = operations.get(i);
+            if (priority < map.get(operation)) {
+                index = i;
+                priority = map.get(operation);
             }
         }
-        return null;
+        return index;
+    }
+
+    private Var calcOneOperation(String leftStr, String operation, String rightStr) throws CalcException {
+        Var right = Var.createVar(rightStr);
+        if (operation.contains("=")) {
+            return Var.save(leftStr, right);
+        }
+        Var left = Var.createVar(leftStr);
+        switch (operation) {
+            case "+":
+                return left.add(right);
+            case "-":
+                return left.sub(right);
+            case "*":
+                return left.mul(right);
+            case "/":
+                return left.div(right);
+        }
+        throw new CalcException("ERROR");
     }
 }
--- src/by/it/leshchenko/calc/Patterns.java
+++ src/by/it/leshchenko/calc/Patterns.java
@@ -1,8 +1,11 @@
 package by.it.leshchenko.calc;
 
 public interface Patterns {
-    String OPERATION = "[-+*/=]";
+    String OPERATION = "(?<=[^-+*/=,{])[-+*/=]";
     String SCALAR = "-?[0-9]+\\.?[0-9]*";
     String VECTOR = "\\{"+ SCALAR + "(," + SCALAR + ")*}";
     String MATRIX = "\\{"+ VECTOR + "(," + VECTOR + ")*}";
+    String BRACKETS = "(?<=\\().+?(?=\\))";
 }
+//D=((40.15-0.15)-20)/(7-5)
+//2+(2*(-3+4/-2-(7-12)--2)+(11*-2)/2)+(4+11)
