df227c7902e829e063cf23f74ebe08ac53c92a48
shepard <www.shepard1992@gmail.com>
2020-12-25 15:16:36 +0300

    Homework jd_02_05 C is started

---
+++ src/by/it/evstratov/calc/language/txt/convertor.bat
@@ -0,0 +1,4 @@
+set path=C:\Program Files\Java\java-se-8u41-ri\bin
+native2ascii -encoding UTF-8 language.txt ..\resources\language.properties
+native2ascii -encoding UTF-8 language_ru_RU.txt ..\resources\language_ru_RU.properties
+native2ascii -encoding UTF-8 language_be_BY.txt ..\resources\language_be_BY.properties
