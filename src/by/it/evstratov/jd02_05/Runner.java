c796b56c9f9389c29450c2a112619a7194872d36
shepard <www.shepard1992@gmail.com>
2020-12-25 13:59:41 +0300

    Homework jd_02_05 A is started

--- src/by/it/evstratov/jd02_05/Runner.java
+++ src/by/it/evstratov/jd02_05/Runner.java
@@ -6,10 +6,10 @@ import java.util.ResourceBundle;
 public class Runner {
 
     public static void main(String[] args) {
-
-        Locale locale = new Locale("ru", "RU");
+        Locale.setDefault(Locale.ENGLISH);
+        Locale locale = new Locale("de", "GE");
         String base = "by.it.evstratov.jd02_05.resources.language";
-        ResourceBundle bundle = ResourceBundle.getBundle(base);
+        ResourceBundle bundle = ResourceBundle.getBundle(base,locale);
         String s = bundle.getString("message.welcome");
         System.out.println(s);
     }
