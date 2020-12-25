1731a576acec828ada7bbfa73621aff03b16ab8b
shepard <www.shepard1992@gmail.com>
2020-12-25 15:04:16 +0300

    Homework jd_02_05 B is completed

--- src/by/it/evstratov/jd02_05/Runner.java
+++ src/by/it/evstratov/jd02_05/Runner.java
@@ -1,24 +1,29 @@
 package by.it.evstratov.jd02_05;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Runner {
 
     public static void main(String[] args) {
         Language lang = Language.INSTANCE;
         Locale.setDefault(Locale.ENGLISH);
-        if(args.length == 2){
-            lang.setLocale(new Locale(args[0], args[1]));
-        }
 
-        System.out.println(Time.getTime(lang.getLocale()));
-        System.out.println(lang.get(Messages.WELCOME));
-        System.out.println(Time.getTime(lang.getLocale()));
-        System.out.println(lang.get(Messages.QUESTION));
-        System.out.println(Time.getTime(lang.getLocale()));
-        System.out.println(lang.get(User.FIRST_NAME));
-        System.out.println(Time.getTime(lang.getLocale()));
-        System.out.println(lang.get(User.LAST_NAME));
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        switch (str){
+            case "ru":
+                lang.setLocale(new Locale("ru","RU"));
+                break;
+            case "be": lang.setLocale(new Locale("be","BY"));
+                break;
+            default: lang.setLocale(new Locale("en","EN"));
+                break;
+        }
+        System.out.println(Time.getTime(lang.getLocale()) + ":" + lang.get(Messages.WELCOME));
+        System.out.println(Time.getTime(lang.getLocale()) + ":" + lang.get(Messages.QUESTION));
+        System.out.println(Time.getTime(lang.getLocale()) + ":" + lang.get(User.FIRST_NAME));
+        System.out.println(Time.getTime(lang.getLocale()) + ":" + lang.get(User.LAST_NAME));
     }
 
 }
