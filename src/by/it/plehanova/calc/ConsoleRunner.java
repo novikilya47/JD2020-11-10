4108217d2dd67511e8f4aa2a234c1e2ff058ea34
Littledrop777 <Littledrop777@gmail.com>
2020-12-25 19:59:22 +0300

    Homework jd02_05 done

--- src/by/it/plehanova/calc/ConsoleRunner.java
+++ src/by/it/plehanova/calc/ConsoleRunner.java
@@ -1,5 +1,8 @@
 package by.it.plehanova.calc;
 
+import by.it.plehanova.jd02_05.Messages;
+import by.it.plehanova.jd02_05.User;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,8 +12,8 @@ public class ConsoleRunner {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH);
 
-        if(args.length==2){
-            lang.setLocale(new Locale(args[0],args[1]));
+        if (args.length == 2) {
+            lang.setLocale(new Locale(args[0], args[1]));
         }
 
         Scanner scanner = new Scanner(System.in);
@@ -20,7 +23,16 @@ public class ConsoleRunner {
         String expression;
         while (true) {
             expression = scanner.nextLine();
-            if (expression.equals("end")) {
+            if (expression.equals("ru")) {
+                lang.setLocale(new Locale("ru", "RU"));
+                System.out.println("Русский язык");
+            } else if (expression.equals("be")) {
+                lang.setLocale(new Locale("be", "BY"));
+                System.out.println("Беларуская мова");
+            } else if (expression.equals("en")) {
+                lang.setLocale(new Locale("en", "UK"));
+                System.out.println("English language");
+            } else if (expression.equals("end")) {
                 break;
             } else if (expression.equals("printvar")) {
                 Var.printVar();
