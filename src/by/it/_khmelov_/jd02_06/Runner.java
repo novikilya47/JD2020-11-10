12bdf951119c515ef8e38dc8d6baae7b3d0e01ec
Alexander Khmelov <akhmelev@gmail.com>
2020-12-26 12:26:27 +0300

    add classwork jd02_06

---
+++ src/by/it/_khmelov_/jd02_06/Runner.java
@@ -0,0 +1,14 @@
+package by.it._khmelov_.jd02_06;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                Logger.getLogger().log("Test ");
+            }).start();
+        }
+
+    }
+}
